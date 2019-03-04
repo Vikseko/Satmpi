60af8ac9722f1f7ddf30362c4be935979cec1d32
Vikseko <vikseko@gmail.com>
2019-03-04 15:00:22 +0800

    minisat-hashmod

---
+++ minisathashmod/core/depend.mk
@@ -0,0 +1,27 @@
+/home/vikseko/Radar_old/minisathashmod/core/Solver.o /home/vikseko/Radar_old/minisathashmod/core/Solver.or /home/vikseko/Radar_old/minisathashmod/core/Solver.od /home/vikseko/Radar_old/minisathashmod/core/Solver.op: /home/vikseko/Radar_old/minisathashmod/core/Solver.cc \
+ /home/vikseko/Radar_old/minisathashmod/mtl/Sort.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/Vec.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/IntTypes.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/XAlloc.h \
+ /home/vikseko/Radar_old/minisathashmod/core/Solver.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/Heap.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/Alg.h \
+ /home/vikseko/Radar_old/minisathashmod/utils/Options.h \
+ /home/vikseko/Radar_old/minisathashmod/utils/ParseUtils.h \
+ /home/vikseko/Radar_old/minisathashmod/core/SolverTypes.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/Map.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/Alloc.h
+/home/vikseko/Radar_old/minisathashmod/core/Main.o /home/vikseko/Radar_old/minisathashmod/core/Main.or /home/vikseko/Radar_old/minisathashmod/core/Main.od /home/vikseko/Radar_old/minisathashmod/core/Main.op: /home/vikseko/Radar_old/minisathashmod/core/Main.cc \
+ /home/vikseko/Radar_old/minisathashmod/utils/System.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/IntTypes.h \
+ /home/vikseko/Radar_old/minisathashmod/utils/ParseUtils.h \
+ /home/vikseko/Radar_old/minisathashmod/utils/Options.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/Vec.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/XAlloc.h \
+ /home/vikseko/Radar_old/minisathashmod/core/Dimacs.h \
+ /home/vikseko/Radar_old/minisathashmod/core/SolverTypes.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/Alg.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/Map.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/Alloc.h \
+ /home/vikseko/Radar_old/minisathashmod/core/Solver.h \
+ /home/vikseko/Radar_old/minisathashmod/mtl/Heap.h
