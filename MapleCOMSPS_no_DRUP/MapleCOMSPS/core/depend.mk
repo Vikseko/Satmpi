15ae134f84b254cf715ed91c87d287da8d11773a
Vikseko <vikseko@gmail.com>
2019-03-05 19:16:23 +0800

    Solvers_for_tests

---
+++ MapleCOMSPS_no_DRUP/MapleCOMSPS/core/depend.mk
@@ -0,0 +1,29 @@
+/home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Main.o /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Main.or /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Main.od /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Main.op: \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Main.cc \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/utils/System.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/IntTypes.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/utils/ParseUtils.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/utils/Options.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/Vec.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/XAlloc.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Dimacs.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/SolverTypes.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/Alg.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/Map.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/Alloc.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Solver.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/Heap.h
+/home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Solver.o /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Solver.or /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Solver.od /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Solver.op: \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Solver.cc \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/Sort.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/Vec.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/IntTypes.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/XAlloc.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/Solver.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/Heap.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/Alg.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/utils/Options.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/utils/ParseUtils.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/core/SolverTypes.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/Map.h \
+ /home/vkondratev/data/Test_solvers3/MapleCOMSPS_no_DRUP/MapleCOMSPS/mtl/Alloc.h
---
+++ MapleLCMDistChronoBT/sources/core/depend.mk
@@ -0,0 +1,11 @@
+/home/vkondratev/data/Test_solvers3/MapleLCMDistChronoBT/sources/core/Main.o /home/vkondratev/data/Test_solvers3/MapleLCMDistChronoBT/sources/core/Main.or /home/vkondratev/data/Test_solvers3/MapleLCMDistChronoBT/sources/core/Main.od /home/vkondratev/data/Test_solvers3/MapleLCMDistChronoBT/sources/core/Main.op: \
+ /home/vkondratev/data/Test_solvers3/MapleLCMDistChronoBT/sources/core/Main.cc \
+ ../utils/System.h ../mtl/IntTypes.h ../utils/ParseUtils.h \
+ ../utils/Options.h ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h \
+ ../core/SolverTypes.h ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h \
+ ../core/Solver.h ../mtl/Heap.h
+/home/vkondratev/data/Test_solvers3/MapleLCMDistChronoBT/sources/core/Solver.o /home/vkondratev/data/Test_solvers3/MapleLCMDistChronoBT/sources/core/Solver.or /home/vkondratev/data/Test_solvers3/MapleLCMDistChronoBT/sources/core/Solver.od /home/vkondratev/data/Test_solvers3/MapleLCMDistChronoBT/sources/core/Solver.op: \
+ /home/vkondratev/data/Test_solvers3/MapleLCMDistChronoBT/sources/core/Solver.cc \
+ ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h \
+ ../core/Solver.h ../mtl/Heap.h ../mtl/Alg.h ../utils/Options.h \
+ ../utils/ParseUtils.h ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h
